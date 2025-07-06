a7baba29df41b6c2dc9ca31d609d0e20c3e2cfcb
bhushan-biradar-75 <bhushanbiradar75@gmail.com>
2025-07-06 16:38:08 +0530

    Debugger config changes & duplicate array programes.

---
+++ index.js
@@ -0,0 +1,48 @@
+// =============================================================================
+// Array Level 0
+
+// Leetcode URl - https://leetcode.com/problems/remove-element/description/
+
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Output: 5, nums = [0,1,4,0,3,_,_,_]
+// Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+// It does not matter what you leave beyond the returned k (hence they are underscores)
+
+function removeElement(nums) {
+  let val = 2;
+  let x = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i] !== val) {
+      nums[x] = nums[i];
+      x = x + 1;
+    }
+  }
+  return x;
+}
+let inputremoveElement = [0, 1, 2, 2, 3, 0, 4, 2];
+let resultremoveElement = removeElement(inputremoveElement);
+console.log("resultremoveElement", resultremoveElement);
+
+// ====================================================================================================
+
+// 26. Remove duplicates from sorted array
+// Leetcode URL - https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+// Input: nums = [0,0,1,1,1,2,2,3,3,4]
+// Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+// Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+// It does not matter what you leave beyond the returned k (hence they are underscores).
+
+function removeDuplicateElement(nums) {
+  let x = 0;
+  for (i = 0; i < nums.length; i++) {
+    if (nums[i + 1] > nums[i]) {
+      nums[x] = nums[i];
+      x = x + 1;
+    }
+  }
+  return x + 1;
+}
+let inputremoveDuplicateElement = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
+let resultremoveDuplicateElement = removeDuplicateElement(inputremoveElement);
+console.log("resultremoveDuplicateElement", resultremoveDuplicateElement);
